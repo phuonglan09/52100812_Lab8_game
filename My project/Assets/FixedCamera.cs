5adb68d9f64b6cf1e4b021b60fb78f27fbdb6d6e
Phuong Lan <52100812@student.tdtu.edu.vn>
2024-04-26 10:47:16 +0700

    lab8

---
+++ My project/Assets/FixedCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour
+{
+    public Transform target; // Người chơi
+    public Vector3 offset; // Khoảng cách tương đối
+
+    void Update()
+    {
+        // Camera di chuyển theo người chơi
+        transform.position = target.position + offset;
+
+        // Tự động xoay camera để nó luôn nằm phía sau người chơi
+        transform.LookAt(target);
+    }
+}
---
+++ My project/Assets/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Tìm vị trí của va chạm
+           // Vector3 collisionPosition = other.transform.position;
+
+            // Đặt vị trí của người chơi thành vị trí của va chạm
+          //  other.transform.position = collisionPosition;
+
+            // Thực hiện các hành động khác nếu cần
+           // Debug.Log("Player hit the obstacle and stopped at collision position: " + collisionPosition);
+        }
+    }
+
+
+}
+
+
